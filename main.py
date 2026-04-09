5091b741c7b2b3aa507efbf1e3ba8eca39d7fee8
unknown <ilayel09@gmail.com>
2026-04-09 20:11:22 +0300

    new main update

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
+from zap_scraper import ZapScraper
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    #the main should only interact with zap parser, or with even a third script such as zap manager that handles both zap parser and zap parser
+    zap_scraper = ZapScraper()
+    product_name = input("enter a product name > ")
+    zap_scraper.fetch_product(product_name)
